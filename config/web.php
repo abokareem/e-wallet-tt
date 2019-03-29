b03149069cf4b4ef2e1b3c855b25c3d31415054e
Farrukh Abdullaev <faraabdullaev@gmail.com>
2019-03-29 19:53:27 +0500

    `config` files updated

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => '2z1e5kUD_5uhBTKQ_CrW6VABt8MaBpCQ',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
